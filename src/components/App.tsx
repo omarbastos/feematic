a5a0a787ac250fb0c2e2d2c159cd24177b359930
Omar Hernandez <ohernandez@hellobuild.co>
2021-12-30 12:31:36 -0400

    Remove autoupdate

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,8 @@
 import Calculator from 'pages/Calculator'
-import { useState } from 'react'
-
-import { registerSW } from 'virtual:pwa-register'
-
-const updateSW = registerSW({
-  onOfflineReady() {}
-})
 
 function App() {
-  const [showUpdateSW, setShowUpdateSW] = useState<boolean>(true)
-  const handleOk = () => {
-    setShowUpdateSW(false)
-    updateSW()
-  }
   return (
     <div className="bg-[#DAF0FF] flex min-w-screen min-h-screen justify-center items-center">
-      {showUpdateSW && (
-        <div className="w-80 px-4 py-2 rounded-lg fixed bottom-0 right-0 flex justify-between items-center bg-gray-300 ">
-          <span className="text-gray-900 text-sm">Ready to work offline</span>
-          <button
-            onClick={handleOk}
-            className="bg-cyan-500 hover:bg-blue text-blue-dark hover:text-white px-4 border border-blue hover:border-transparent rounded"
-          >
-            OK
-          </button>
-        </div>
-      )}
       <Calculator />
     </div>
   )
