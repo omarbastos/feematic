cbbbcdcd6c3ee1d3945f4e4072e44eb7ae528b9c
Omar Hernandez <ohernandez@hellobuild.co>
2022-02-24 18:36:22 -0400

    hotfix vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,36 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'Feematic',
+        short_name: 'Feematic',
+        description: 'Exchange Rate Calculator',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
       registerType: 'autoUpdate',
       workbox: {
         cleanupOutdatedCaches: false,
