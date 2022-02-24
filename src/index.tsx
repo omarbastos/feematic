f7d585359f6b9ecae4756866d72cda57e3d1c60a
Omar Hernandez <ohernandez@hellobuild.co>
2022-02-24 18:41:01 -0400

    hotifx: autoinvoce service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import 'tailwindcss/tailwind.css'
 import App from 'App'
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 
 render(
   <StrictMode>
